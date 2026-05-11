9e6d039f316fbe0e65629fa7afa9ea46166eb2fc
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-05-11 17:15:10 +0300

    Add changes to applied pointer types, remove 'clickability' from decorative page elements

--- src/ritual.js
+++ src/ritual.js
@@ -2,36 +2,27 @@ import crystalBallImage from "./assets/img/crystalBall.png";
 
 let ritualDisplay = function() {
 
-    //add a crystal ball which will show a random fortune - add about 14 variants for variety, maybe should be styled ad taro cards?
-
     let content = document.querySelector("#content");
 
     let crystalBall = document.createElement("img");
     crystalBall.src = crystalBallImage;
     crystalBall.id = 'ball';
 
-    let contactBlock = document.createElement("div");
-    contactBlock.classList.add("contact-text");
+    let upperRitualTextBlock = document.createElement("div");
+    upperRitualTextBlock.classList.add("ritual-text");
+    let bottomRitualTextBlock = document.createElement("div");
+    bottomRitualTextBlock.classList.add("ritual-text");
 
     let addLine = function(text) {
         let newLine = document.createElement("div");
         newLine.textContent = text;
-
-        if (text.includes(":") && !text.includes(" ")) {
-            newLine.classList.add("contact-title");
-        } 
-
-        contactBlock.appendChild(newLine);
+        upperRitualTextBlock.appendChild(newLine);
     }
 
-    let normalText = function() {
-        addLine("Test text to distinguish contacts");
-        addLine("This is a second line");
-    }
-
-    normalText();
+    addLine("Дозвольте кулі пророцтв зазирнути у вашу долю та обрати смак цієї магічної ночі.");
+    content.appendChild(upperRitualTextBlock);
     content.appendChild(crystalBall);
-    content.appendChild(contactBlock);
+    content.appendChild(bottomRitualTextBlock);
 
 }
 
